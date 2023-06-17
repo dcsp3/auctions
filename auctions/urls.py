15f8139e1a46d7e834a0aa7eb8558428fc6117dc
dcsp3 <daksheshpandey@gmail.com>
2023-06-17 18:47:32 +0530

    implement watchlist functionality

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create", views.create, name="create"),
-    path("listing/<str:id>", views.listing, name="listing")
+    path("listing/<str:id>", views.listing, name="listing"),
+    path('watchlist/add/<str:listing_id>/', views.add_to_watchlist, name='add_to_watchlist'),
 ]
