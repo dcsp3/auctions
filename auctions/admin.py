1bc9c1dbab32dfe19afeffb36cd80f8992f3bbbb
dcsp3 <daksheshpandey@gmail.com>
2023-06-17 18:46:43 +0530

    register watchlist model

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Listings, Comments
+from .models import Listings, Comments, Watchlist
 
 # Register your models here.
 
 admin.site.register(Listings)
 admin.site.register(Comments)
+admin.site.register(Watchlist)
